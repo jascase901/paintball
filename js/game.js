654d525551688ebcb39bc77a89acf84d32ca3a05
Jason <jason@jason-770Z5E-780Z5E-870Z5E-880Z5E.(none)>
2014-02-12 19:22:02 -0800

    added bullets

--- js/game.js
+++ js/game.js
@@ -5,6 +5,11 @@ canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
+DUP=0;
+DDOWN=1;
+DLEFT=2;
+DRIGHT=3;
+
 // Background image
 var bgReady = false;
 var bgImage = new Image();
@@ -21,6 +26,17 @@ heroImage.onload = function () {
 };
 heroImage.src = "images/hero.png";
 
+var paintballReady = false;
+
+var paintballImage = new Image();
+paintballImage.onload = function () {
+	paintballReady = true;
+};
+paintballImage.src = "images/paintball.png";
+
+
+
+
 // Monster image
 var monsterReady = false;
 var monsterImage = new Image();
@@ -33,6 +49,14 @@ monsterImage.src = "images/monster.png";
 var hero = {
 	speed: 256 // movement in pixels per second
 };
+var paintball= {
+	speed: 256 // movement in pixels per second
+};
+var paintballs= []
+paintballcount = 0;
+
+
+
 var monster = {};
 var monstersCaught = 0;
 
@@ -61,18 +85,57 @@ var reset = function () {
 var update = function (modifier) {
 	if (38 in keysDown) { // Player holding up
 		hero.y -= hero.speed * modifier;
+		hero.direction = DUP;
 	}
 	if (40 in keysDown) { // Player holding down
 		hero.y += hero.speed * modifier;
+		hero.direction = DDOWN;
 	}
 	if (37 in keysDown) { // Player holding left
 		hero.x -= hero.speed * modifier;
+		hero.direction = DLEFT;
 	}
 	if (39 in keysDown) { // Player holding right
 		hero.x += hero.speed * modifier;
+		hero.direction = DRIGHT;
+	}
+	if (32 in keysDown){//Player is pressing Space
+		paintball.x = hero.x;
+		paintball.y = hero.y;
+		
+		//append bullet to bullet list
+		paintballs.push({x: hero.x, y: hero.y,direction: hero.direction});
+		paintballcount++;
 	}
 
 	// Are they touching?
+	//This code but with each bullet
+	for(var i=0; i<paintballcount; i++)
+	{
+		switch(paintballs[i].direction)
+		{
+			case DUP:
+				console.log("up");
+				paintballs[i].y=*10*modifier;
+				break;
+			case DDOWN:
+				console.log("down");
+				paintballs[i].y=*-10*modifier;
+				break;
+			case DLEFT:
+				console.log("left");
+				paintballs[i].x=*-10*modifier;
+				break;
+			case DRIGHT:
+				console.log("right");
+				paintballs[i].x=10*modifier;
+				break;
+			defualt:
+				alert("FUCL");
+				break;
+		}
+
+	}
 	if (
 		hero.x <= (monster.x + 32)
 		&& monster.x <= (hero.x + 32)
@@ -93,6 +156,15 @@ var render = function () {
 	if (heroReady) {
 		ctx.drawImage(heroImage, hero.x, hero.y);
 	}
+
+	if (paintballReady) {
+		for(var i=0;i<paintballcount;i++)
+		{
+			ctx.drawImage(paintballImage, paintballs[i].x, paintballs[i].y);
+		}
+	}
+	//for each in bullet ids 
+	//ctx.drawImage(bulletImage, each.x, each.y);
 
 	if (monsterReady) {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
